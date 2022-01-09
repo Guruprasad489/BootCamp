e3a431e921717936106552db5c1250d553e98b99
Guruprasad489 <guruprasadk.dev@gmail.com>
2022-01-09 23:57:31 +0530

    Calculate daily employee wage

--- D9_EmpWage_UC.sh
+++ D9_EmpWage_UC.sh
@@ -3,11 +3,18 @@
 echo Welcome to Employee Wage Computation 
 
 ispresent=$((RANDOM%2))
+perHourSalary=20;
+workingHour=0;
 
   if [ $ispresent -eq 1 ]
   then
     echo "Employee is Present"
+    workingHour=0;
   elif [ $ispresent -eq 0 ]
     then
     echo "Employee is Absent"
+    workingHour=8;
   fi
+
+salary=$(($perHourSalary * $workingHour));
+echo "Employee has earned $salary $ today";
