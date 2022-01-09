eb022c6602e530662a7dec2163c854bc1a16c9da
Guruprasad489 <guruprasadk.dev@gmail.com>
2022-01-09 23:17:36 +0530

    Check Employee is Presenr or Absent

--- D9_EmpWage_UC.sh
+++ D9_EmpWage_UC.sh
@@ -2,3 +2,12 @@
 
 echo Welcome to Employee Wage Computation 
 
+ispresent=$((RANDOM%2))
+
+  if [ $ispresent -eq 1 ]
+  then
+    echo "Employee is Present"
+  elif [ $ispresent -eq 0 ]
+    then
+    echo "Employee is Absent"
+  fi
